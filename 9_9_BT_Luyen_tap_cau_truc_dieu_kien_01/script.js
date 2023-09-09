12e82104e70c6e2f8c990d7c122419b7f8e64c6d
huynguyenngocdang <danghuy9229@gmail.com>
2023-09-09 11:25:26 +0700

    clean coding bt if 1

--- 9_9_BT_Luyen_tap_cau_truc_dieu_kien_01/script.js
+++ 9_9_BT_Luyen_tap_cau_truc_dieu_kien_01/script.js
@@ -1,61 +1,65 @@
 function functionBai1() {
   let a = document.getElementById("a1").value;
   let b = document.getElementById("b1").value;
+  let textResult = "";
 
   if (a % b == 0) {
-    document.getElementById("resultBai1").innerHTML = "A chia hết cho B";
+    textResult = "A chia hết cho B";
   } else {
-    document.getElementById("resultBai1").innerHTML = "A không chia hết cho B";
+    textResult = "A không chia hết cho B";
   }
+  document.getElementById("resultBai1").innerHTML = textResult;
 }
 
 function functionBai2() {
   let age = document.getElementById("age").value;
+  let textResult = "";
 
   if (age >= 14 && age <= 17) {
-    document.getElementById("resultBai2").innerHTML = "Được nhập học";
+    textResult = "Được nhập học";
   } else {
-    document.getElementById("resultBai2").innerHTML = "Không được nhập học";
+    textResult = "Không được nhập học";
   }
+  document.getElementById("resultBai2").innerHTML = textResult;
 }
 
 function functionBai3() {
   let number = document.getElementById("numberWhole").value;
+  let textResult = "";
   switch (true) {
     case number > 0:
-      document.getElementById("resultBai3").innerHTML = "Số lớn hơn 0";
+      textResult = "Số lớn hơn 0";
       break;
     case number < 0:
-      document.getElementById("resultBai3").innerHTML = "Số bé hơn 0";
+      textResult = "Số bé hơn 0";
       break;
     case number == 0:
-      document.getElementById("resultBai3").innerHTML = "Số bằng 0";
+      textResult = "Số bằng 0";
       break;
     default:
-      document.getElementById("resultBai3").innerHTML = "Không xác định";
+      textResult = "Không xác định";
       break;
   }
+  document.getElementById("resultBai3").innerHTML = textResult;
 }
 
 function functionBai4() {
   let number1 = document.getElementById("number1").value;
   let number2 = document.getElementById("number2").value;
   let number3 = document.getElementById("number3").value;
+  let status = "";
   if (number1 > number2) {
     if (number1 > number3) {
-      document.getElementById("resultBai4").innerHTML =
-        "Số thứ 1 (" + number1 + ") là lớn nhất";
+      status = "Số thứ 1 (Giá trị: " + number1 + ") là lớn nhất";
     } else {
-      document.getElementById("resultBai4").innerHTML =
-        "Số thứ 3 (" + number3 + ") là lớn nhất";
+      status = "Số thứ 3 (Giá trị: " + number3 + ") là lớn nhất";
     }
   } else if (number2 > number3) {
-    document.getElementById("resultBai4").innerHTML =
-      "Số thứ 2 (" + number2 + ") là lớn nhất";
+    status = "Số thứ 2 (Giá trị: " + number2 + ") là lớn nhất";
   } else {
-    document.getElementById("resultBai4").innerHTML =
-      "Số thứ 3 (" + number3 + ") là lớn nhất";
+    status = "Số thứ 3 (Giá trị: " + number3 + ") là lớn nhất";
   }
+  document.getElementById("resultBai4").innerHTML = status;
 }
 
 function functionBai5() {
@@ -64,50 +68,53 @@ function functionBai5() {
   let averageMth = document.getElementById("averageMth").value;
   let averageLit = document.getElementById("averageLit").value;
   let averageEng = document.getElementById("averageEng").value;
+  let status = "";
 
   if (averageTotal >= 8) {
     if (averageLowest >= 6.5) {
       if (averageMth >= 8 || averageLit >= 8 || averageEng >= 8) {
-        document.getElementById("resultBai5").innerHTML = "Học lực giỏi";
+        status = "Học lực giỏi";
       } else {
-        document.getElementById("resultBai5").innerHTML = "Học lực khá";
+        status = "Học lực khá";
       }
     } else {
-      document.getElementById("resultBai5").innerHTML = "Học lực khá";
+      status = "Học lực khá";
     }
   } else if (averageTotal >= 6.5) {
     if (averageLowest >= 5) {
       if (averageMth >= 6.5 || averageLit >= 6.5 || averageEng >= 6.5) {
-        document.getElementById("resultBai5").innerHTML = "Học lực khá";
+        status = "Học lực khá";
       } else {
-        document.getElementById("resultBai5").innerHTML = "Học lực trung bình";
+        status = "Học lực trung bình";
       }
     } else {
-      document.getElementById("resultBai5").innerHTML = "Học lực trung bình";
+      status = "Học lực trung bình";
     }
   } else if (averageTotal >= 5.0) {
     if (averageLowest >= 3.5) {
       if (averageMth >= 5 || averageLit >= 5 || averageEng >= 5) {
-        document.getElementById("resultBai5").innerHTML = "Học lực trung bình";
+        status = "Học lực trung bình";
       } else {
-        document.getElementById("resultBai5").innerHTML = "Học lực yếu";
+        status = "Học lực yếu";
       }
     } else {
-      document.getElementById("resultBai5").innerHTML = "Học lực yếu";
+      status = "Học lực yếu";
     }
   } else if (averageTotal >= 3.5) {
     if (averageLowest >= 2) {
       if (averageMth >= 3.5 || averageLit >= 3.5 || averageEng >= 3.5) {
-        document.getElementById("resultBai5").innerHTML = "Học lực yếu";
+        status = "Học lực yếu";
       } else {
-        document.getElementById("resultBai5").innerHTML = "Học lực kém";
+        status = "Học lực kém";
       }
     } else {
-      document.getElementById("resultBai5").innerHTML = "Học lực kém";
+      status = "Học lực kém";
     }
   } else {
-    document.getElementById("resultBai5").innerHTML = "Học lực kém";
+    status = "Học lực kém";
   }
+
+  document.getElementById("resultBai5").innerHTML = status;
 }
 
 function functionBai6() {
@@ -116,27 +123,27 @@ function functionBai6() {
   let muc1 = 1.5 / 100;
   let muc2 = 3 / 100;
   let muc3 = 4 / 100;
-  let commisionAmount;
+  let commisionAmount, statusLevel, statusResult;
 
   if (revenue >= 800) {
     commisionAmount = revenue * muc3;
-    document.getElementById("commissionLevel").innerHTML = "Mức 3: ";
-    document.getElementById("resultBai6").innerHTML =
-      commisionAmount + " triệu đồng";
+    statusLevel = "Mức 3: ";
+    statusResult = commisionAmount + " triệu đồng";
   } else if (revenue >= 500) {
     commisionAmount = revenue * muc2;
-    document.getElementById("commissionLevel").innerHTML = "Mức 2: ";
-    document.getElementById("resultBai6").innerHTML =
-      commisionAmount + " triệu đồng";
+    statusLevel = "Mức 2: ";
+    statusResult = commisionAmount + " triệu đồng";
   } else if (revenue >= 280) {
     commisionAmount = revenue * muc1;
-    document.getElementById("commissionLevel").innerHTML = "Mức 1: ";
-    document.getElementById("resultBai6").innerHTML =
-      commisionAmount + " triệu đồng";
+    statusLevel = "Mức 1: ";
+    statusResult = commisionAmount + " triệu đồng";
   } else {
-    document.getElementById("commissionLevel").innerHTML = "Mức 0: ";
-    document.getElementById("resultBai6").innerHTML = " 0 triệu đồng";
+    statusLevel = "Mức 0: ";
+    statusResult = " 0 triệu đồng";
   }
+
+  document.getElementById("commissionLevel").innerHTML = statusLevel;
+  document.getElementById("resultBai6").innerHTML = statusResult;
 }
 
 function functionBai7() {
@@ -153,42 +160,54 @@ function functionBai7() {
 
   if (dienTieuThu <= 50) {
     tienDien = dienTieuThu * mucDien1;
-    vat = tienDien * 8 /100;
+    vat = (tienDien * 8) / 100;
     total = tienDien + vat;
     document.getElementById("mucDien").innerHTML = "Mức 0: ";
-
   } else if (dienTieuThu <= 100) {
-    tienDien = (dienTieuThu-50) * mucDien2 + mucDien1*50;
-    vat = tienDien * 8 /100;
+    tienDien = (dienTieuThu - 50) * mucDien2 + mucDien1 * 50;
+    vat = (tienDien * 8) / 100;
     total = tienDien + vat;
     document.getElementById("mucDien").innerHTML = "Mức 2: ";
-
   } else if (dienTieuThu <= 200) {
-    tienDien = (dienTieuThu-100) * mucDien3 + mucDien1*50 + mucDien2*50;
-    vat = tienDien * 8 /100;
+    tienDien = (dienTieuThu - 100) * mucDien3 + mucDien1 * 50 + mucDien2 * 50;
+    vat = (tienDien * 8) / 100;
     total = tienDien + vat;
     document.getElementById("mucDien").innerHTML = "Mức 3: ";
-
   } else if (dienTieuThu <= 300) {
-    tienDien = (dienTieuThu-200) * mucDien4 + mucDien1*50 + mucDien2*50 + mucDien3*100;
-    vat = tienDien * 8 /100;
+    tienDien =
+      (dienTieuThu - 200) * mucDien4 +
+      mucDien1 * 50 +
+      mucDien2 * 50 +
+      mucDien3 * 100;
+    vat = (tienDien * 8) / 100;
     total = tienDien + vat;
     document.getElementById("mucDien").innerHTML = "Mức 4: ";
-
   } else if (dienTieuThu <= 400) {
-    tienDien = (dienTieuThu-300) * mucDien5 + mucDien1*50 + mucDien2*50 + mucDien3*100+ mucDien4*100;
-    vat = tienDien * 8 /100;
+    tienDien =
+      (dienTieuThu - 300) * mucDien5 +
+      mucDien1 * 50 +
+      mucDien2 * 50 +
+      mucDien3 * 100 +
+      mucDien4 * 100;
+    vat = (tienDien * 8) / 100;
     total = tienDien + vat;
     document.getElementById("mucDien").innerHTML = "Mức 5: ";
-
   } else {
-    tienDien = (dienTieuThu-400) * mucDien6 + mucDien1*50 + mucDien2*50 + mucDien3*100+ mucDien4*100+mucDien5*100;
-    vat = tienDien * 8 /100;
+    tienDien =
+      (dienTieuThu - 400) * mucDien6 +
+      mucDien1 * 50 +
+      mucDien2 * 50 +
+      mucDien3 * 100 +
+      mucDien4 * 100 +
+      mucDien5 * 100;
+    vat = (tienDien * 8) / 100;
     total = tienDien + vat;
     document.getElementById("mucDien").innerHTML = "Mức 6: ";
-
   }
-  document.getElementById("resultBai7").innerHTML = new Intl.NumberFormat().format(tienDien) + " VND";
-  document.getElementById("vat").innerHTML = new Intl.NumberFormat().format(vat) + " VND";
-  document.getElementById("total").innerHTML = new Intl.NumberFormat().format(total) + " VND";
+  document.getElementById("resultBai7").innerHTML =
+    new Intl.NumberFormat().format(tienDien) + " VND";
+  document.getElementById("vat").innerHTML =
+    new Intl.NumberFormat().format(vat) + " VND";
+  document.getElementById("total").innerHTML =
+    new Intl.NumberFormat().format(total) + " VND";
 }
