05e321da08d2de323062692761e4a5672f4db375
huynguyenngocdang <danghuy9229@gmail.com>
2023-09-11 08:49:45 +0700

    commit fake textarea

---
+++ 13_4_BT_Ung_dung_faking_textarea/script.js
@@ -0,0 +1,21 @@
+const textTyping =
+  "[Bài tập] Ứng dụng vui Faking Textarea on Typing. Mô tả chức năng: Khi người dùng gõ bàn phím thì một dòng chữ cố định sẽ lần lượt hiện ra. Cho dù người dùng gõ chữ nào thì cũng lần lượt hiện ra dòng chữ cố định đó.";
+
+let txtArray = Array.from(textTyping);
+let resultPrint = "";
+console.log(txtArray)
+
+function fakeText() {
+  let inputLength = document.getElementById("txtarea").value.length;
+
+  for (let index = 0; index < inputLength; index++) {
+    resultPrint += txtArray[index];
+    if (index == 214) {
+      document.getElementById("txtarea").value = "";
+      resultPrint = "";
+    }
+  }
+  document.getElementById("txtarea").value = resultPrint;
+  resultPrint = "";
+  setTimeout("fakeText()", 1);
+}
