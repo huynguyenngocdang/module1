d2bcee6027a2797cd9bba8328df3ad956a460782
huynguyenngocdang <danghuy9229@gmail.com>
2023-09-12 15:49:32 +0700

    commit 15_4_BT_LuyenTapHam2

--- 15_4_BT_Luyen_tap_tao_va_su_dung_ham_02/script.js
+++ 15_4_BT_Luyen_tap_tao_va_su_dung_ham_02/script.js
@@ -92,3 +92,115 @@ function tinhGiaiThua(value) {
   }
   return thisVal;
 }
+
+function bai4Function(value) {
+  let result = document.getElementById("resultBai4")
+
+  if (isNumber(value)) {
+    result.innerHTML = "TRUE";
+  } else {
+    result.innerHTML = "FALSE";
+  }
+}
+
+function isNumber(value) {
+  return !isNaN(parseFloat(value)) && isFinite(value)
+}
+
+function bai5Function() {
+  let a = document.getElementById("inputBai5A").value;
+  let b = document.getElementById("inputBai5B").value;
+  let c = document.getElementById("inputBai5C").value;
+  let result = checkSmallest(a, b, c);
+  document.getElementById("resultBai5").innerHTML = result;
+}
+
+function checkSmallest(a, b, c) {
+  let result = "";
+   if (a < b) {
+      if (a < c) {
+        result = "A is smallest";
+      } else {
+        result = "C is smallest" ;
+      }
+   } else if(b < c) {
+      result = "B is smallest";
+   } else {
+    result = "C is smallest";
+   }
+
+   return result;
+}
+
+function bai6Function(value) {
+  let result = ""
+  if (checkLarger0(value) && checkWholeNumber(value)) {
+    result = "Đây là số nguyên dương"
+  } else {
+    result = "Đây không phải số nguyên dương"
+  }
+  document.getElementById("resultBai6").innerHTML = result;
+}
+
+function checkWholeNumber(value) {
+  let isWhole = (value - Math.floor(value) == 0);
+  if (isWhole) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkLarger0(value) {
+  return value > 0;
+}
+
+function bai7Function() {
+  let a = document.getElementById("inputBai7A").value;
+  let b = document.getElementById("inputBai7B").value;
+  let temp;
+  temp = a;
+  a = b;
+  b = temp;
+  document.getElementById("inputBai7A").value = a;
+  document.getElementById("inputBai7B").value = b;
+}
+
+function bai8Function() {
+  let stringInput = document.getElementById("inputBai8").value.toString();
+  let arrayBai8 = [];
+  let arrayReverse = [];
+
+  arrayBai8 = stringInput.split(",").map(function (item) {
+    return item.trim();
+  });
+
+  for (let index = arrayBai8.length-1 ; index >= 0; index--) {
+    arrayReverse.push(arrayBai8[index]);
+    
+  }
+  document.getElementById("inputBai8").value = arrayReverse;
+}
+
+function bai9Function() {
+  let stringInput = document.getElementById("inputBai9Array").value.toString();
+  let searchInput = document.getElementById("inputBai9Char").value;
+  let arrayBai9 = [];
+  let count = 0;
+  arrayBai9 = stringInput.split(",").map(function (item) { return item.trim()});
+
+  for (let i = 0; i < arrayBai9.length; i++) {
+    if (arrayBai9[i].length > 0) {
+        for (let j = 0; j < arrayBai9[i].length; j++) {
+          if(arrayBai9[i].charAt(j) == searchInput)
+          count += 1;
+        }
+    } 
+    
+  }
+  if (count > 0) {
+    document.getElementById("resultBai9").innerHTML = count;
+  } else {
+    document.getElementById("resultBai9").innerHTML = "-1";
+  }
+}
--- 9_6_BT_Puzzle/script.js
+++ 9_6_BT_Puzzle/script.js
@@ -1,72 +1,3 @@
-// function changeImg(source, id) {
-//   let img1 = document.getElementById("object1");
-//   let img2 = document.getElementById("object2");
-//   let img3 = document.getElementById("object3");
-//   let srcImg1 = img1.src;
-//   let srcImg2 = img2.src;
-//   let srcImg3 = img3.src;
-//   let posFirst;
-//   let posLast;
-
-//   let textStart = "image/";
-//   let textEnd = ".png";
-//   if (checkPic(srcImg1, srcImg2, srcImg3)) {
-//     alert("Correct");
-//   }
-
-//   if (id == "object1") {
-//     posFirst = parseInt(srcImg1.charAt(srcImg1.length - 7));
-//     posLast = parseInt(srcImg1.charAt(srcImg1.length - 5));
-//     if (posFirst == 3) {
-//       posFirst = 1;
-//       img1.src = textStart + posFirst + "_" + posLast + textEnd;
-//     } else {
-//       posFirst += 1;
-//       img1.src = textStart + posFirst + "_" + posLast + textEnd;
-//     }
-//     console.log(posLast);
-//   }
-//   if (id == "object2") {
-//     posFirst = parseInt(srcImg2.charAt(srcImg2.length - 7));
-//     posLast = parseInt(srcImg2.charAt(srcImg2.length - 5));
-//     if (posFirst == 3) {
-//       posFirst = 1;
-//       img2.src = textStart + posFirst + "_" + posLast + textEnd;
-//     } else {
-//       posFirst += 1;
-//       img2.src = textStart + posFirst + "_" + posLast + textEnd;
-//     }
-//     console.log(posLast);
-//   }
-//   if (id == "object3") {
-//     posFirst = parseInt(srcImg3.charAt(srcImg3.length - 7));
-//     posLast = parseInt(srcImg3.charAt(srcImg3.length - 5));
-//     if (posFirst == 3) {
-//       posFirst = 1;
-//       img3.src = textStart + posFirst + "_" + posLast + textEnd;
-//     } else {
-//       posFirst += 1;
-//       img3.src = textStart + posFirst + "_" + posLast + textEnd;
-//     }
-//     console.log(posLast);
-//   }
-
-//   if (checkPic(srcImg1, srcImg2, srcImg3)) {
-//     alert("Correct");
-//   }
-// }
-
-// function checkPic(obj1, obj2, obj3) {
-//   let posFirstObj = parseInt(obj1.charAt(obj1.length - 7));
-//   let posSecondObj = parseInt(obj2.charAt(obj2.length - 7));
-//   let posThirdObj = parseInt(obj3.charAt(obj3.length - 7));
-//   if (posFirstObj == posSecondObj && posSecondObj == posThirdObj) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
 let img1 = document.getElementById("object1");
 let img2 = document.getElementById("object2");
 let img3 = document.getElementById("object3");
@@ -74,18 +5,22 @@ let array1 = ["image/1_1.png", "image/2_1.png", "image/3_1.png"];
 let array2 = ["image/1_2.png", "image/2_2.png", "image/3_2.png"];
 let array3 = ["image/1_3.png", "image/2_3.png", "image/3_3.png"];
 
+
+//random interger
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//random image on load page
 function randomImage() {
   img1.src = array1[parseInt(getRandomInt(0, 2)).toFixed(0)];
   img2.src = array2[parseInt(getRandomInt(0, 2)).toFixed(0)];
   img3.src = array3[parseInt(getRandomInt(0, 2)).toFixed(0)];
 }
 
+//change image on click
 function changeImg(src, id) {
   let text = src.substring(src.length - 13);
   let currentPos;
@@ -122,6 +57,7 @@ function changeImg(src, id) {
   check();
 }
 
+//check if puzzle correct
 function check() {
   let img1 = document.getElementById("object1");
   let img2 = document.getElementById("object2");
@@ -132,8 +68,11 @@ function check() {
   index1 = index1.substring(index1.length - 13);
   index2 = index2.substring(index2.length - 13);
   index3 = index3.substring(index3.length - 13);
-  
-  if (array1.indexOf(index1) == array2.indexOf(index2) && array2.indexOf(index2)  == array3.indexOf(index3)) {
+
+  if (
+    array1.indexOf(index1) == array2.indexOf(index2) &&
+    array2.indexOf(index2) == array3.indexOf(index3)
+  ) {
     alert("You are correct");
   }
 
